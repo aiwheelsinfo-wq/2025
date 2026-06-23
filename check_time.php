31eab6bef9c177fa329aedaf709ee0cebff1532d
Agni car <agnicarrental@gmail.com>
2026-06-23 10:40:13 +0530

    Update check_time MD5

--- check_time.php
+++ check_time.php
@@ -3,4 +3,5 @@ header("Content-Type: text/plain");
 echo "PHP Default Timezone: " . date_default_timezone_get() . "\n";
 echo "PHP Date: " . date("Y-m-d H:i:s T") . "\n";
 echo "System Date: " . shell_exec("date") . "\n";
+echo "JSON file MD5: " . md5_file('./agni-car-app-firebase-adminsdk-fbsvc-4f70f7d1f2.json') . "\n";
 ?>
