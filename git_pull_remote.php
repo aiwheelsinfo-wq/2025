1a47e97b0538bab3c38b71d68a9a9ff39f1083a1
Agni car <agnicarrental@gmail.com>
2026-06-23 23:33:08 +0530

    Execute fix_json_key.php sequentially in the same shell_exec command

--- git_pull_remote.php
+++ git_pull_remote.php
@@ -3,7 +3,7 @@ header("Content-Type: text/plain");
 echo "Fetching latest from origin...\n";
 echo shell_exec("git fetch origin 2>&1");
 echo "Force resetting to origin/main...\n";
-echo shell_exec("git reset --hard origin/main 2>&1");
+echo shell_exec("git reset --hard origin/main && php fix_json_key.php 2>&1");
 echo "Status after reset:\n";
 echo shell_exec("git log -1 --oneline 2>&1");
 
