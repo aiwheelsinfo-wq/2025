082ea911a742a24bb58a5c6fffe501cfd1aa4a32
Agni car <agnicarrental@gmail.com>
2026-06-26 11:00:28 +0530

    Disable display_errors in updatePayment.php to prevent PHP 8.5 deprecation warnings

--- updatePayment.php
+++ updatePayment.php
@@ -2,9 +2,8 @@
 header("Content-Type: application/json");
 include 'db_connect.php'; // your DB connection file
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 0);
 
 
 $response = [];
