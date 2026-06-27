439830c3859343d5d0fedeb98392f8993e6923cb
Agni car <agnicarrental@gmail.com>
2026-06-27 11:24:57 +0530

    temp: test email alert directly

--- test_email.php
+++ test_email.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 include 'db_connect.php';
 include 'notification_helper.php';
 
-echo "Sending booking notification for ID 1...\n";
-$res = sendBookingWhatsAppNotification(1, $conn);
+echo "Sending email alert directly to ansiansu27@gmail.com...\n";
+$res = sendEmailAlert('ansiansu27@gmail.com', 'Direct Email Test', '<h1>This is a direct test email</h1>', 'Test User');
 echo "Result:\n";
 var_dump($res);
 ?>
