4ad51b850db0c747aa209c607fedbdc49a3c0486
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-08-17 10:33:57 +0530

    Add CORS headers and OPTIONS preflight to updatePayment.php and get_invoice_data.php

--- updatePayment.php
+++ updatePayment.php
@@ -1,5 +1,14 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 include 'db_connect.php'; // your DB connection file
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
